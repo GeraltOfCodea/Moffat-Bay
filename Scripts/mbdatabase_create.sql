004ccad86ddba190a98a56e50dfef6a9c0af44b0
GaraltofCodea <rnorrbom@uncloudtech.com>
2024-11-10 08:32:18 -0800

    Updated the Create DB sql script

--- Scripts/mbdatabase_create.sql
+++ Scripts/mbdatabase_create.sql
@@ -1,42 +1,48 @@
+-- Fresh database creation
 DROP DATABASE IF EXISTS MB_DATABASE;
 CREATE DATABASE MB_DATABASE;
 
-CREATE TABLE MB_DATABASE . registeredUsers (
-    customer_id int(5) NOT NULL AUTO_INCREMENT,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    email_address varchar(50) NOT NULL,
-    password_hash varchar(255) NOT NULL,
-    phone_number varchar(15)  NOT NULL,
+-- Table to store user information after registration
+CREATE TABLE MB_DATABASE.registered_users (
+    customer_id INT(3) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email_address VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15)  NOT NULL,
     PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE MB_DATABASE . rooms (
-    room_id int(5) NOT NULL AUTO_INCREMENT,
-    room_size varchar(99) NOT NULL,
+-- Table to store room types
+CREATE TABLE MB_DATABASE.rooms (
+    room_id INT(3) NOT NULL AUTO_INCREMENT,
+    room_size VARCHAR(99) NOT NULL,
     room_price DECIMAL(6, 2),
     PRIMARY KEY (room_id)
 );
 
-CREATE TABLE  MB_DATABASE . bookings (
-    booking_id int(5) NOT NULL AUTO_INCREMENT,
-    customer_id int(5) NOT NULL,
-    number_of_guests integer,
-    order_date timestamp,
-    check_in timestamp,
-    check_out timestamp,
+-- Table to store confirmed user bookings
+CREATE TABLE  MB_DATABASE.bookings (
+    booking_id INT(3) NOT NULL AUTO_INCREMENT,
+    customer_id INT(3) NOT NULL,
+    number_of_guests INT,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    check_in TIMESTAMP,
+    check_out TIMESTAMP,
     PRIMARY KEY (booking_id),
-    FOREIGN KEY (customer_id) REFERENCES registeredUsers (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES registered_users (customer_id)
 );
 
-CREATE TABLE MB_DATABASE . bookedRooms (
-    booking_id int(5) NOT NULL,
-    room_id int(5) NOT NULL,
+-- Table to store rooms that are booked to track availability
+CREATE TABLE MB_DATABASE. booked_rooms (
+    booking_id INT(3) NOT NULL,
+    room_id INT(3) NOT NULL,
     PRIMARY KEY (booking_id),
     FOREIGN KEY (booking_id) REFERENCES bookings (booking_id)
 );
 
-CREATE TABLE MB_DATABASE.contactUs (
+-- Table to store requests/messages from guests and users
+CREATE TABLE MB_DATABASE.contact_us (
     inquiry_id INT(5) NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     email_address VARCHAR(50) NOT NULL,
